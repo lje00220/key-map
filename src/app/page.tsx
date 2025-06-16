bd825a41de6f3c70498c90fa383ff278e8c291f7
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-06-16 19:29:30 +0900

    feat:#5 메인 페이지에 사이드바 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <div></div>;
-}
+import Sidebar from '@/components/Sidebar';
+
+const Home = () => {
+  return (
+    <main className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1">
+        <h1>메인 영역</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
